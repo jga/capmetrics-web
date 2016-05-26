14e6dc526b2c8495866e598d3043572e7b24c6e6
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-05-25 23:31:31 -0500

    Add system-trend model.

---
+++ app/models/system-trend.js
@@ -0,0 +1,37 @@
+/**
+ * Provides convenient trend data for a *type* of transit service.
+ *
+ * ### **Model Fields**
+ *
+ * | Field        | Type       |
+ * |--------------|------------|
+ * | `updatedOn`  | Date       |
+ * | `serviceType`| String     |
+ * | `trend`      | JSON       |
+ *
+ * @module models/system-trend
+ */
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+
+/**
+ * Exports an exension of the Ember Data `Model` class.
+ */
+export default Model.extend({
+    /** When the model was last updated.
+     *
+     * @type {Date}
+     */
+    updatedOn: attr('date'),
+    /** JSON with trend data for a service type.
+     *
+     * @type {JSON}
+     */
+    trend: attr(),
+    /**
+     * A description of the type of transit service (e.g. 'Bus' or 'Rail').
+     *
+     * @type {String}
+     */
+    serviceType: attr('string')
+});
