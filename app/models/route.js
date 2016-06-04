80e718155ccc88681040a54cd2bdb873605e9429
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-06-04 10:24:58 -0500

    Fill in route model fields.

--- app/models/route.js
+++ app/models/route.js
@@ -1,4 +1,11 @@
 import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
+  routeNumber: attr('number'),
+  routeName: attr('string'),
+  serviceType: attr('string'),
+  isHighRidership: attr('boolean'),
+  dailyRiderships: hasMany('daily-ridership')
 });
