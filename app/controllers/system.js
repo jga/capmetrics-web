e40650de6c37ffd6a897f5f9ada0494c6a063e40
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-06-07 12:11:07 -0500

    Add system route.

---
+++ app/controllers/system.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  /** A CSS identifier selector.
+   *
+   * Must not include the hash/pound at the start.
+   *
+   * @type {String}
+   */
+  systemVizSelector: 'system-system-ridership-viz'
+});
