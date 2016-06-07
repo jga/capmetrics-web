8c7f1b4bd1c79297e2fe0c61a103e4b4f910ad39
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-06-07 12:08:05 -0500

    Create importable service colors object.

---
+++ app/utils/service-colors.js
@@ -0,0 +1,16 @@
+export default {
+  'CORE - METRORAIL': 'red',
+  'CORE - METRORAPID': '#f24d6e',
+  'CORE - LIMITED': '#649dce',
+  'CORE - RADIAL': '#5190c8',
+  'CORE - FEEDER': '#3d84c2',
+  'CORE - EXPRESS/FLYERS': '#3777ae',
+  'CORE - CROSSTOWN': '#77a9d4',
+  'SPECIAL - REVERSE COMMUTE': '#6c9452',
+  'SPECIAL - RAIL CONNECTORS': '#4b874b',
+  'SPECIAL - EBUS': '#42775b',
+  'SPECIAL - NIGHT OWLS': '#7ec87e',
+  'SPECIAL - SENIOR': '#70b18e',
+  'UT - CIRCULATORS': '#ffbb4f',
+  'UT - RADIALS': '#af823c'
+}
