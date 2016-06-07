d3146115ffb6dc76eece16fed951a292a1e27cb0
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-06-07 12:01:03 -0500

    Update routes.

--- app/router.js
+++ app/router.js
@@ -18,6 +18,10 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('index', {path: '/'});
+  this.route('route-ridership', {path: '/route-ridership/:route_number'});
+  this.route('routes');
+  this.route('system');
+  this.route('about');
 });
 
 export default Router;
