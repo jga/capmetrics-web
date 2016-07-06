5b93bb99a087a8ed4b571bfdea443e4e9e59e548
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-07-06 12:17:14 -0500

    Clean up ridership numbers by normalizing as round numbers.

---
+++ app/serializers/system-trend.js
@@ -0,0 +1,17 @@
+import JSONAPISerializer from 'ember-data/serializers/json-api';
+
+export default JSONAPISerializer.extend({
+  normalize(modelClass, resourceHash) {
+    let originalTrend = JSON.parse(resourceHash.attributes.trend);
+    let cleanTrend = []
+    for (let i = 0; i < originalTrend.length; i++) {
+      // each original 'pair' is an iso datetime string 'paired' with a ridership float
+      let originalPair = originalTrend[i];
+      let ridership = Math.round(originalPair[1]);
+      let cleanPair = [originalPair[0], ridership];
+      cleanTrend.push(cleanPair);
+    }
+    resourceHash.attributes.trend = cleanTrend;
+    return this._super(...arguments);
+  },
+});
