989bd29938fe860ea862fe914254d1373a6422da
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-07-07 13:11:01 -0500

    Add new Mirage route handlers.

--- app/mirage/route-handlers/index.js
+++ app/mirage/route-handlers/index.js
@@ -1,12 +1,15 @@
 import getDailyRiderships from './get-daily-riderships';
+import getHighRidership from './get-high-ridership';
 import getRoute from './get-route';
 import getRoutes from './get-routes';
+import getRouteLabels from './get-route-labels';
 import getServiceHourRiderships from './get-service-hour-riderships';
 import getSystemTrends from './get-system-trends';
 
 export {getDailyRiderships as getDailyRiderships};
-export {getServiceHourRiderships as getServiceHourRiderships};
+export {getHighRidership as getHighRidership};
 export {getRoute as getRoute};
 export {getRoutes as getRoutes};
+export {getRouteLabels as getRouteLabels};
+export {getServiceHourRiderships as getServiceHourRiderships};
 export {getSystemTrends as getSystemTrends};
-
