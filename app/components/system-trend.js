fc5c869a383df218d9016284c37a72c8af087958
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-07-19 11:41:32 -0500

    Remove system-trend component-specific utility modules and place in component.

--- app/components/system-trend.js
+++ app/components/system-trend.js
@@ -4,9 +4,126 @@
  * @module components/viz-area-trend
  */
 import Ember from 'ember';
-import toStackedChart from 'capmetrics-web/utils/stacked-chart';
-import colorizeTrends from 'capmetrics-web/utils/colorize-trends';
 import fillEmptyPoints from 'capmetrics-web/utils/fill-empty-points';
+import transitServiceColors from 'capmetrics-web/utils/service-colors';
+
+let colorizeTrends = function(datum) {
+  for (let i = 0; i < datum.length; i++) {
+    let data = datum[i];
+    let color = 'purple';
+    if (transitServiceColors.hasOwnProperty(data.key)) {
+      color = transitServiceColors[data.key];
+    }
+    data.color = color;
+  }
+  return datum;
+}
+
+// Transforms ISO 8601 timestamps provided by Capmetrics API into
+// more easily readable strings for chart x-axis ticks.
+let convertTimeStamps = function(d){
+  let result = Date.parse(d[0]);
+  return result;
+}
+
+let configureChart = function(chart) {
+  // set mobile-first defaults
+  let chartHeight = 250;
+  let marginTop = 10;
+  let marginBottom = 10;
+  let marginLeft = 50;
+  let marginRight = 50;
+  let showControls = false;
+  let showLegend = false;
+  // adjust settings based on window size
+  let size = nv.utils.windowSize();
+  if (size.width >= 768) {
+    chartHeight = 400;
+    marginBottom = 50;
+    showControls = true;
+    showLegend = true;
+  }
+  // set initial render chart design
+  chart.margin({
+    top: marginTop,
+    right: marginRight,
+    bottom: marginBottom,
+    left: marginLeft
+  });
+  chart.height(chartHeight);
+  chart.showControls(showControls);
+  chart.showLegend(showLegend);
+  chart.legend.margin({bottom: 20});
+  return chart;
+}
+
+let getContainerHeight = function() {
+  let size = nv.utils.windowSize();
+  if (size.width >= 768) {
+    return 400;
+  } else {
+    return 300;
+  }
+}
+
+let createGraphLoader = function(selector, data, chart) {
+  // sort keys in ascending alphabetical order
+  data.sort(function(a, b) {
+    if (a.key > b.key) { return 1 }
+    if (a.key < b.key) { return -1 }
+    return 0;
+  });
+  chart = configureChart(chart);
+  let svgContainerHeight = getContainerHeight();
+  return function loader() {
+    try {
+      d3.select(selector).append('svg')
+        .datum(data)
+        .transition().duration(500)
+        .call(chart)
+        .style({'height': svgContainerHeight + 'px'});
+      let manager = nv.utils.windowResize(function() {
+        chart = configureChart(chart);
+        chart.update();
+        let newHeight = getContainerHeight();
+        d3.select(selector + ' .nvd3-svg')
+          .style({'height': newHeight + 'px'});
+      });
+      chart.clear = manager.clear;
+      return chart;
+    } catch (e) {
+      if (e instanceof TypeError) {
+        console.log('TypeError for ' + selector);
+      } else {
+        console.log('Error for ' + selector);
+      }
+      console.log(e);
+    }
+  }
+}
+
+/**
+ * Handles the creation of a stacked chart visualization.
+ *
+ * @inner
+ * @param {string} title A chart title
+ * @param {string} selector An id CSS selector
+ * @param {array} data An array of data points for visualization.
+ */
+let toStackedChart = function(title, selector, data) {
+  let chart = nv.models
+                .stackedAreaChart()
+                .useInteractiveGuideline(true)
+                .x(convertTimeStamps)
+                .y(function(d) { return parseInt(d[1]) })
+                .controlLabels({ stacked: "Stacked" })
+                .duration(300);
+  chart.legend.vers('furious');
+  chart.xAxis.tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+  chart.yAxis.tickFormat(d3.format(','));
+  nv.addGraph(createGraphLoader(('#' + selector), data, chart));
+  return chart;
+}
 
 /** Exports an extension of Ember's Component class. */
 export default Ember.Component.extend({
