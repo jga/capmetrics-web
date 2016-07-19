205a0fb4a962ab80582ce4c1f016828c9958d1d2
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-07-19 11:20:46 -0500

    Sort AJAX response with high ridership route data for index by highest weekday ridership.

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,8 +10,38 @@ let topRouteHandler = function() {
   let ajaxSettings = {
     url: ENV.APP.API_HOST + '/high-ridership',
   }
-  return Ember.$.ajax(ajaxSettings).done(function(data) {
-    return data;
+  return Ember.$.ajax(ajaxSettings).done(function(routeCompendiums) {
+      // sort descending by latest weekday ridership - highest weekday ridership
+      // route will be at the start of array
+      routeCompendiums.sort(function(a, b) {
+        //get last season and calendar year - capmetrics ETL places most recent timestamps at end of array
+        var mostRecentYear = a.riderships[a.riderships.length - 1].calendarYear;
+        var mostRecentSeason = a.riderships[a.riderships.length - 1].season;
+        //get 'weekday' with last timestamp
+        let ridershipA = _.find(a.riderships, function(ridership) {
+          return (ridership.calendarYear === mostRecentYear
+                  && ridership.season === mostRecentSeason
+                  && ridership.dayOfWeek === 'weekday')
+        })
+        let ridershipB = _.find(b.riderships, function(ridership) {
+          return (ridership.calendarYear === mostRecentYear
+                  && ridership.season === mostRecentSeason
+                  && ridership.dayOfWeek === 'weekday')
+        })
+        if (ridershipA) {
+          if (ridershipA.ridership > ridershipB.ridership) {
+            return -1;
+          }
+          if (ridershipA.ridership < ridershipB.ridership) {
+            return 1;
+          }
+          return 0;
+        } else {
+          console.log('An expected "weekday" DailyRidership model was NOT found for the top routes.')
+          return 1;
+        }
+      })
+      return routeCompendiums;
   })
 }
 
